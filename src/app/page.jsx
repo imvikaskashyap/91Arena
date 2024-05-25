fa2c6e90066572bfa55799e37186170e44e651f5
imvikaskashyap <mvikaskashyap@gmail.com>
2024-05-25 23:43:28 +0530

    all done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { DarkMode } from "@/custom-components/Theme-Mode";
 import Blog from "@/components/blog";
-// import Blog2 from "@/components/blog2";
 import { TabsDemo } from "@/components/allSmartphone&FInder";
 import LatestMobiles from "@/components/latestMobiles";
 import UpcomingMobiles from "@/components/upcomingMobiles";
 import RecentlyViewed from "@/components/recentlyViewed";
 import NewMobiles from "@/components/newMobiles";
 import BlogTemplate from "@/components/blogTemplate";
-// import Carousel from "@/components/slider";
 const page = () => {
   return (
     <div className="m-2 lg:m-8">
       <div className="flex flex-col gap-6 items-center justify-between my-8 lg:flex-row lg:h-[89vh]">
         <TabsDemo />
-        {/* <PhoneFinderContainer/> */}
         <Blog />
       </div>
       <RecentlyViewed/>
