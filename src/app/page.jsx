892aec4846ff53f6ecf05284d36bf0f5ed7dc990
imvikaskashyap <mvikaskashyap@gmail.com>
2024-05-26 00:36:36 +0530

    ok

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Blog from "@/components/blog";
-import TabsDemo  from "@/components/allSmartphone&FInder";
 import LatestMobiles from "@/components/latestMobiles";
 import UpcomingMobiles from "@/components/upcomingMobiles";
 import RecentlyViewed from "@/components/recentlyViewed";
@@ -8,11 +7,12 @@ import NewMobiles from "@/components/newMobiles";
 import LatestNews from "@/components/latestNews";
 import LatestHindiNews from "@/components/latestHindiNews";
 import LatestReview from "@/components/latestReview";
+import TabsDemo from "@/components/allSmartphone&FInder";
 const page = () => {
   return (
     <div className="m-2 lg:m-8">
       <div className="flex flex-col gap-6 items-center justify-between my-8 lg:flex-row lg:h-[89vh]">
-        <TabsDemo />
+        <TabsDemo/>
         <Blog />
       </div>
       <RecentlyViewed/>
