fc8634b903a8f20fedd89da792a4acf20938751f
imvikaskashyap <mvikaskashyap@gmail.com>
2024-05-26 23:58:06 +0530

    specification page done

--- src/app/specification/page.jsx
+++ src/app/specification/page.jsx
@@ -15,7 +15,8 @@ import {
   BadgeIndianRupee,
   ImageDown,
   Columns2,
-  
+  PlugZap2,
+  BatteryCharging,
 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -91,177 +92,227 @@ const specifications = {
     SAR: "0.93 W/kg (head)",
   },
 };
+const lightenColor = (color, percent) => {
+  let R = parseInt(color.substring(1, 3), 16);
+  let G = parseInt(color.substring(3, 5), 16);
+  let B = parseInt(color.substring(5, 7), 16);
+
+  R = parseInt((R * (100 + percent)) / 100);
+  G = parseInt((G * (100 + percent)) / 100);
+  B = parseInt((B * (100 + percent)) / 100);
+
+  R = R < 255 ? R : 255;
+  G = G < 255 ? G : 255;
+  B = B < 255 ? B : 255;
+
+  const RR = R.toString(16).length === 1 ? `0${R.toString(16)}` : R.toString(16);
+  const GG = G.toString(16).length === 1 ? `0${G.toString(16)}` : G.toString(16);
+  const BB = B.toString(16).length === 1 ? `0${B.toString(16)}` : B.toString(16);
+
+  return `#${RR}${GG}${BB}`;
+};
 
 const SpecificationPage = () => {
+  const primaryColor = "#4A90E2"; // Replace this with the primary color extracted from the image
+  const lightPrimaryColor = lightenColor(primaryColor, 50); // Lighten the primary color by 50%
+
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="w-full max-w-3xl mx-auto">
-        <div className="border rounded-lg bg-white shadow-md flex flex-col justify-between">
-        <div className="w-full h-11 bg-gray-200"><h1 className="px-4 text-2xl font-bold">Samsung Galaxy S24</h1></div>
-         <div className="flex items-center justify-between">
-         <div className="">
-            <Image
-              src="/assets/images/samsung-galaxy-s24-ultra-5g-sm-s928-stylus.jpg"
-              alt="Smartphone"
-              className="mx-2 h-60 object-contain"
-              width={160}
-              height={212}
-            />
+      <div className="w-full max-w-[800px] mx-auto">
+        <div
+          style={{
+            background: `linear-gradient(-45deg, ${primaryColor} 0%, #ffffff 100%)`,
+          }}
+          className="border rounded-lg bg-white shadow-md flex flex-col justify-between"
+        >
+          <div className="w-full h-11  border-b ">
+            <h1 className="px-4 text-2xl text-primary font-bold">Samsung Galaxy S24</h1>
           </div>
+          <div className="flex items-center justify-between">
+            <div className="">
+              <Image
+                src="/assets/images/google-pixel-8-pro.jpg"
+                alt="Smartphone"
+                className="mx-2 h-60 object-contain"
+                width={160}
+                height={212}
+              />
+            </div>
 
-          <div className="flex flex-col gap-4 w-1/3 border-r p-2">
-            <div>
-              <li className="text-gray-700 flex items-center gap-2 mb-2">
-                <i className=" text-gray-500 ">
-                   
-                  <CalendarCheck className="w-4 h-4" />
-                </i>
-                <span className="saira-FontFamily text-[#2b2b2] text-sm">
-                  Released 2024, January 24
-                </span>
-              </li>
-              <li className="text-gray-700 flex items-center text-sm gap-2 space-x-2 mb-2">
-                <i className=" text-gray-500">
-                   
-                  <Smartphone className="w-4 h-4" />
-                </i>
-                <span className="saira-FontFamily text-[#2b2b2] text-sm">
-                  232g or 233g, 8.6mm thickness
-                </span>
-              </li>
-              <li className="text-gray-700 flex items-center text-sm gap-2 space-x-2 mb-2">
-                <i className="text-gray-500">
-                   
-                  <ChevronsLeftRight className="w-4 h-4" />
-                </i>
-                <span className="saira-FontFamily text-[#2b2b2] text-sm">
-                  Android 14, One UI 6.1
-                </span>
-              </li>
-              <li className="text-gray-700 flex items-center gap-2 text-sm space-x-2 mb-2">
-                <i className=" text-gray-500">
-                  <MemoryStick className="w-4 h-4" />
-                </i>
-                <span className="saira-FontFamily text-[#2b2b2] text-sm">
-                  256GB/512GB/1TB storage, no card slot
-                </span>
-              </li>
+            <div className="flex flex-col gap-4 border-r py-2 ps-0 pe-3">
+              <div>
+                <li className="text-gray-700 flex items-center gap-2 mb-2">
+                  <i className="text-gray-500">
+                    <CalendarCheck className="w-4 h-4" />
+                  </i>
+                  <span className="saira-FontFamily text-[#2b2b2] text-sm tracking-wider">
+                    Released 2024, January 24
+                  </span>
+                </li>
+                <li className="text-gray-700 flex items-center text-sm gap-2 space-x-2 mb-2">
+                  <i className="text-gray-500">
+                    <Smartphone className="w-4 h-4" />
+                  </i>
+                  <span className="saira-FontFamily text-[#2b2b2] text-sm tracking-wider">
+                    232g or 233g, 8.6mm thickness
+                  </span>
+                </li>
+                <li className="text-gray-700 flex items-center text-sm gap-2 space-x-2 mb-2">
+                  <i className="text-gray-500">
+                    <ChevronsLeftRight className="w-4 h-4" />
+                  </i>
+                  <span className="saira-FontFamily text-[#2b2b2] text-sm tracking-wider">
+                    Android 14, One UI 6.1
+                  </span>
+                </li>
+                <li className="text-gray-700 flex items-center gap-2 text-sm space-x-2 mb-2">
+                  <i className="text-gray-500">
+                    <MemoryStick className="w-4 h-4" />
+                  </i>
+                  <span className="saira-FontFamily text-[#2b2b2] text-sm tracking-wider">
+                    256GB/512GB/1TB storage, no card slot
+                  </span>
+                </li>
               </div>
 
-            <div className="flex items-center justify-between">
-              <div className=" flex flex-col items-start justify-between rounded-lg">
-                <i className="">
-                  
-                  <Square />
-                </i>
-                <strong className="block text-pink-600 text-xl mt-1">
-                  <span>6.8</span>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col items-start justify-between p-2 rounded-lg   ">
+                  <i className="">
+                    <Square />
+                  </i>
+                  <strong className="block text-pink-600 text-xl mt-1 tracking-wider">
+                    <span>6.8</span>
+                  </strong>
+                  <p className="text-gray-600 text-sm tracking-wider">
+                    1440x3120 pixels
+                  </p>
+                </div>
+                <div className="flex flex-col items-start justify-between rounded-lg p-2   ">
+                  <i className="text-lg">
+                    <Aperture />
+                  </i>
+                  <strong className="block text-orange-500 text-xl mt-1">
+                    <span className="tracking-wider">200</span>
+                    <span className="tracking-wider">MP</span>
+                  </strong>
+                  <div className="text-gray-600 text-sm tracking-wider">
+                    4320p
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-1/6 flex flex-col justify-between items-center gap-4 border-r py-2 ps-0 pe-3">
+              <div className="text-center rounded-lg mb-4">
+                <strong className="text-pink-600 flex items-center gap-2 text-xl">
+                  <i className="text-lg">
+                    <TrendingUp />
+                  </i>
+                  <span className="text-3xl">57%</span>
                 </strong>
-                <p className="text-gray-600 text-sm">1440x3120 pixels</p>
+                <span className="text-gray-600 text-sm">5,535,450 hits</span>
               </div>
               <div className="flex flex-col items-start justify-between rounded-lg">
-                <i className=" text-lg">
-                   
-                  <Aperture />
+                <i className="text-lg">
+                  <Cpu />
                 </i>
-                <strong className="block text-orange-500 text-xl mt-1">
-                  <span>200</span>
-                  <span>MP</span>
+                <strong className="text-blue-600 text-sm block mt-1">
+                  <span className="text-xl">12 GB</span> RAM
                 </strong>
-                <div className="text-gray-600 text-sm">4320p</div>
+                <div className="text-gray-600">
+                  <p className="text-sm">Snapdragon 8 Gen 3</p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="w-1/5 flex flex-col justify-between items-center gap-4  border-r p-2">
-            <div className="text-center p-4 bg-gray-100 rounded-lg mb-4">
-              <strong className="text-pink-600 flex items-center gap-2 text-xl">
-                <i className=" text-lg">
-                  <TrendingUp />
+            <div className="w-1/6 flex flex-col justify-between items-center gap-8 py-2 ps-0 pe-3">
+              <div className="text-center p-4    rounded-lg mb-4">
+                <a className="text-pink-600 hover:text-pink-700" href="#">
+                  <strong className="flex items-center justify-start gap-4 text-xl">
+                    <i className="text-lg">
+                      <Heart />
+                    </i>
+                    <span className="text-3xl">976</span>
+                  </strong>
+                  <span className="text-sm">Become a fan</span>
+                </a>
+              </div>
+              <div className="flex flex-col items-start justify-between p-2    rounded-lg">
+                <i className="text-lg">
+                  <BatteryMedium />
                 </i>
-                <span className="text-3xl">57%</span>
-              </strong>
-              <span className="text-gray-600">5,535,450 hits</span>
-            </div>
-            <div className="flex flex-col items-start justify-between p-3 bg-gray-100 rounded-lg ">
-              <i className=" text-lg">
-                <Cpu />
-              </i>
-              <strong className="text-blue-600 text-xl block mt-1">
-                <span>12</span> GB RAM
-              </strong>
-              <div className="text-gray-600" data-spec="chipset-hl">
-                Snapdragon 8 Gen 3
-              </div>
-            </div>
-          </div>
-
-          <div className="w-1/5">
-            <div className="text-center p-4 bg-gray-100 rounded-lg mb-4">
-              <a className=" text-pink-600 hover:text-pink-700" href="#">
-                <strong className="flex items-center justify-start gap-2 text-xl">
-                  <i className="text-lg">
-                   
-                    <Heart />
-                  </i>
-                 <span className="text-3xl ">976</span> 
+                <strong className="text-green-600 text-sm block mt-1">
+                  <span className="text-2xl">5000</span>{" "}
+                  <span className="text-xl">mAh</span>
                 </strong>
-                <span>Become a fan</span>
-              </a>
-            </div>
-            <div className="flex flex-col items-start justify-between p-4 bg-gray-100 rounded-lg">
-              <i className=" text-lg">
-                <BatteryMedium />
-              </i>
-              <strong className="text-green-600 text-xl block mt-1">
-                <span>5000</span> mAh
-              </strong>
-              <div className="text-gray-600">
-                <i className=" mr-2"></i>45W
-                <i className=" ml-4"></i>15W
+                <div className="text-gray-600 flex justify-between items-center">
+                  <div className="flex items-center text-sm mr-2">
+                    <i className="">
+                      <PlugZap2 className="w-4 h-4" />
+                    </i>
+                    45W
+                  </div>
+                  <div className="flex items-center text-sm">
+                    <BatteryCharging className="w-4 h-4" /> <i className=""></i>
+                    15W
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-         </div>
-        <div className="w-full flex items-center justify-between h-11 bg-gray-100">
-          <Button variant="secondary"  className="w-36 rounded-none border-e hover:bg-gray-300">
-          <ImageDown className="mr-1 w-5 h-5"/>
-            Pictures
-          </Button>
-          <Button variant="secondary" className="w-36 rounded-none border-e hover:bg-gray-300">
-          <Columns2 className="mr-1 w-5 h-5" />
-            Compare
-          </Button>
-          <Button variant="secondary" className="w-36 rounded-none border-e hover:bg-gray-300">
-          <Eye className="mr-1 w-5 h-5" /> 
-          Reviews
-          </Button>
-          <Button variant="secondary" className="w-36 rounded-none border-e hover:bg-gray-300">
-          <MessageSquareText className="-mb-2 mr-1 w-5 h-5"/>
-            Opinion
-          </Button>
-          <Button variant="secondary" className="w-36 rounded-none hover:bg-gray-300">
-          <BadgeIndianRupee className="mr-1 w-5 h-5"/>
-            Prices
-          </Button>
+          <div className="w-full flex items-center justify-between h-11 border-t  py-2 ps-0 pe-3">
+            <Button
+              variant="secondary"
+              className="w-36 rounded-none border-e bg-transparent"
+            >
+              <ImageDown className="mr-1 w-5 h-5" />
+              Pictures
+            </Button>
+            <Button
+              variant="secondary"
+              className="w-36 rounded-none border-e hover:bg-gray-300 bg-transparent"
+            >
+              <Columns2 className="mr-1 w-5 h-5" />
+              Compare
+            </Button>
+            <Button
+              variant="secondary"
+              className="w-36 rounded-none border-e hover:bg-gray-300 bg-transparent"
+            >
+              <Eye className="mr-1 w-5 h-5" />
+              Reviews
+            </Button>
+            <Button
+              variant="secondary"
+              className="w-36 rounded-none border-e hover:bg-gray-300 bg-transparent"
+            >
+              <MessageSquareText className="-mb-2 mr-1 w-5 h-5" />
+              Opinion
+            </Button>
+            <Button
+              variant="secondary"
+              className="w-36 rounded-none hover:bg-gray-300 bg-transparent"
+            >
+              <BadgeIndianRupee className="mr-1 w-5 h-5" />
+              Prices
+            </Button>
+          </div>
         </div>
 
-        </div>
-
-        <div className="mt-8">
+        <div className="mt-2 borderCss p-3">
           {Object.entries(specifications).map(([section, details]) => (
             <div key={section} className="mb-8">
-              <h2 className="text-2xl font-bold mb-4">
+              <h2 className="text-2xl font-bold mb-1">
                 {section.toUpperCase()}
               </h2>
               <table className="table-auto w-full text-left">
                 <tbody>
                   {Object.entries(details).map(([key, value]) => (
                     <tr key={key} className="border-b">
-                      <th className="px-4 py-2 font-medium text-gray-800">
+                      <th className="px-2 py-1 font-medium text-gray-800">
                         {key}
                       </th>
-                      <td className="px-4 py-2 text-gray-600">
+                      <td className="px-2 py-1 text-gray-600">
                         {typeof value === "boolean"
                           ? value
                             ? "Yes"
